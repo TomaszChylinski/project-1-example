023057d1a0cb302c5892a1f5092c39d4d762dba7
chylinski.t <chylinski.t@outlook.com>
2019-12-01 21:05:40 -0500

    firebase succesfully added

--- assets/javascript/firebase-logic.js
+++ assets/javascript/firebase-logic.js
@@ -1,17 +1,18 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyChhYquphq9TwGjBw2eDltnRKXRcaHiN_c",
     authDomain: "filmbase-847bf.firebaseapp.com",
     databaseURL: "https://filmbase-847bf.firebaseio.com",
     projectId: "filmbase-847bf",
     storageBucket: "filmbase-847bf.appspot.com",
     messagingSenderId: "54580608272",
-    appId: "1:54580608272:web:009e7b0f84b8b2336e7995",
-    measurementId: "G-DHNWN5ZBFT"
+    appId: "1:54580608272:web:248cc3ca697223346e7995",
+    measurementId: "G-TV2F9TKHEL"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
 
 
 //reference the database 
